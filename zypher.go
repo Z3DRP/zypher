5fe5c906e36175acaeaf61a27d32deae4fa8a32e
Zach Palmer <apex1421@outlook.com>
2024-09-04 15:35:35 -0500

    zypHash, Zyph stubbed hexShift and shift added

--- zypher.go
+++ zypher.go
@@ -1,24 +1,31 @@
 package zypher
 
 import (
+	"crypto/sha512"
+	"encoding/hex"
 	"errors"
 	"regexp"
 	"sync"
+	"unicode"
 )
 
 type Zypher struct {
-	Shift       int  // number of runes/digits to shift, defaults to 3
-	IterCount   int  // number of iterations to be applied to value being zyphered, defaults to 3
-	Alternate   bool // if true when odd elements will reverse shift, defaults false
-	IgnoreSpace bool // if true will ignore space and leave them in, defaults to false
+	Shift             int  // number of runes/digits to shift, defaults to 3
+	ShiftIterCount    int  // number of iterations to be applied to value being zyphered, defaults to 3
+	HashIterCount     int  // number of iterations to be hashed, defaults to 3
+	Alternate         bool // if true when odd elements will reverse shift, defaults false
+	IgnoreSpace       bool // if true will ignore space and leave them in, defaults to false
+	RestrictHashShift bool // if true will only shift hash values within hahs digit range if false will shift digits outside hex range ie f could shift to j, default false
 }
 
 func DefaultZops() *Zypher {
 	return &Zypher{
-		Shift:       3,
-		Alternate:   false,
-		IterCount:   3,
-		IgnoreSpace: false,
+		Shift:             3,
+		Alternate:         false,
+		ShiftIterCount:    3,
+		HashIterCount:     3,
+		IgnoreSpace:       false,
+		RestrictHashShift: false,
 	}
 }
 
@@ -42,9 +49,15 @@ func WithAlternate(wrp bool) func(*Zypher) {
 	}
 }
 
-func WithIterCount(count int) func(*Zypher) {
+func WithShiftIterCount(count int) func(*Zypher) {
 	return func(z *Zypher) {
-		z.IterCount = count
+		z.ShiftIterCount = count
+	}
+}
+
+func WithHashIterCount(count int) func(*Zypher) {
+	return func(z *Zypher) {
+		z.HashIterCount = count
 	}
 }
 
@@ -54,18 +67,24 @@ func WithIgnoreSpace(i bool) func(*Zypher) {
 	}
 }
 
+func WithRestrictedHashShift(i bool) func(*Zypher) {
+	return func(z *Zypher) {
+		z.RestrictHashShift = i
+	}
+}
+
 func (z Zypher) AsciZyph(arg string) (string, error) {
 	isValidString := regexp.MustCompile(`^[a-zA-Z0-9 ]+$`).MatchString(arg)
 
 	if !isValidString {
-		InvalidString := errors.New("only strings containing numbers, letters, and spaces are allowd")
+		InvalidString := errors.New("invalid string, only strings containing numbers, letters, and spaces are allowd")
 		return "", InvalidString
 	}
 
 	result := make([]rune, len(arg))
 	var wg sync.WaitGroup
 
-	for i := 0; i < z.IterCount; i++ {
+	for i := 0; i < z.ShiftIterCount; i++ {
 		for indx, r := range arg {
 			wg.Add(1)
 			go func() {
@@ -80,6 +99,47 @@ func (z Zypher) AsciZyph(arg string) (string, error) {
 	}
 	wg.Wait()
 	return string(result), nil
+}
+
+func (z Zypher) HexZyph(arg string) (string, error) {
+	isValidString := regexp.MustCompile(`^[a-fA-F0-9\s]+$`).MatchString(arg)
+	if !isValidString {
+		InvalidString := errors.New("invalid hex string, only A-F, a-f, and 0-9 allowed")
+		return " ", InvalidString
+	}
+	// TODO HexZyph takes a hash then shifts it shifts for each shiftItercount
+
+}
+
+func (z Zypher) Zyph(arg string) (string, error) {
+	isValidString := regexp.MustCompile(`^[a-zA-Z0-9!.,'"?_=\-+@#$%&()\s]+$`).MatchString(arg)
+	if !isValidString {
+		InvalidString := errors.New(`invalid string, only strings containing alpha numerics, spaces, and symbols ! . , ' " ? _ = - + @ # $ % & () are allowed`)
+		return "", InvalidString
+	}
+
+	// TODO Zyph shifts string then hashes it foreach shiftItercount and foreach hashItercount
+}
+
+func (z Zypher) ZypHash(arg string) (string, error) {
+	isValidString := regexp.MustCompile(`^[a-zA-Z0-9!.,'"?_=\-+@#$%&()\s]+$`).MatchString(arg)
+
+	if !isValidString {
+		InvalidString := errors.New(`invalid string, only strings containing alpha numerics, spaces, and symbols ! . , ' " ? _ = - + @ # $ % & () are allowed`)
+		return " ", InvalidString
+	}
+
+	result := arg
+	for i := 0; i < z.HashIterCount; i++ {
+		hsh := sha512.New()
+		// sidx := len(result) - 4
+		// slt := result[sidx:]
+		hsh.Write([]byte(result))
+		bs := hsh.Sum(nil)
+		result = hex.EncodeToString(bs)
+	}
+
+	return result, nil
 }
 
 func asciShift(target *rune, r rune, shf int, alt bool, ignSpc bool) {
@@ -102,8 +162,61 @@ func asciShift(target *rune, r rune, shf int, alt bool, ignSpc bool) {
 	} else if r == ' ' {
 		if !ignSpc {
 			*target = 'x'
+			// *target = 32 + (r-32+rune(shf)+95)%95
 		} else {
 			*target = ' '
+		}
+	} else {
+		*target = r
+	}
+}
+
+func hexShift(target *rune, r rune, shf int, alt, ignSpc bool) {
+	switch alt {
+	case true:
+		if shf > 0 {
+			shf = -shf
+		} else {
+			shf = +shf
+		}
+	}
+
+	if r >= '0' && r <= '9' {
+		*target = '0' + (r-'0'+rune(shf)+10)%10
+	} else if r >= 'a' && r <= 'f' {
+		*target = 'a' + (r-'a'+rune(shf)+6)%6
+	} else if r >= 'A' && r <= 'F' {
+		*target = 'A' + (r-'A'+rune(shf)+6)%6
+	} else if r >= 32 && r <= 126 {
+		if r == 32 && ignSpc {
+			*target = r
+		} else {
+			*target = 32 + (r-32+rune(shf)+95)%95
+		}
+	}
+}
+
+func shift(target *rune, r rune, shf int, alt, ignSpc bool) {
+	switch alt {
+	case true:
+		if shf > 0 {
+			shf = -shf
+		} else {
+			shf = +shf
+		}
+	}
+
+	if unicode.IsDigit(r) {
+		*target = '0' + (r-'0'+rune(shf)+10)%10
+	} else if unicode.IsUpper(r) {
+		*target = 'A' + (r-'A'+rune(shf)+26)%26
+	} else if unicode.IsLower(r) {
+		*target = 'a' + (r-'a'+rune(shf)+26)%26
+	} else if r >= 32 && r <= 126 {
+		if r == 32 && ignSpc {
+			*target = r
+		} else {
+			*target = 32 + (r-32+rune(shf)+95)%95
 		}
 	}
 }
--- zypher_test.go
+++ zypher_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAsciZyphNoIgnoreSpace(t *testing.T) {
 	v := "Abc Z19"
-	zy := NewZypher(WithIterCount(1))
+	zy := NewZypher(WithShiftIterCount(1))
 	reslt, _ := zy.AsciZyph(v)
 	want := "DefxC42"
 
@@ -15,11 +15,110 @@ func TestAsciZyphNoIgnoreSpace(t *testing.T) {
 
 func TestAsciZyphIgnoreSpace(t *testing.T) {
 	v := "Abc Z19"
-	zy := NewZypher(WithIterCount(1), WithIgnoreSpace(true))
+	zy := NewZypher(WithShiftIterCount(1), WithIgnoreSpace(true))
 	reslt, _ := zy.AsciZyph(v)
 	want := "Def C42"
 
 	if reslt != want {
 		t.Errorf("got %q, wanted %q", reslt, want)
+	}
+}
+
+func TestDefaultZops(t *testing.T) {
+	zy := DefaultZops()
+	want := wantZyph(3, 3, 3, false, false)
+
+	if zy.Shift != want.shift {
+		t.Errorf("got shift %q, wanted shift %q", zy.Shift, want.shift)
+	}
+
+	if zy.ShiftIterCount != want.shfIterCount {
+		t.Errorf("got shfitercount %q, wanted shfitercount %q", zy.ShiftIterCount, want.shfIterCount)
+	}
+
+	if zy.HashIterCount != want.hshIterCount {
+		t.Errorf("got hshitercount %v, wanted hshitercount %v", zy.HashIterCount, want.hshIterCount)
+	}
+
+	if zy.Alternate != want.alternate {
+		t.Errorf("got alternate %v, wanted alternate %v", zy.Alternate, want.alternate)
+	}
+
+	if zy.IgnoreSpace != want.ignoreSpace {
+		t.Errorf("got ignorespace %v, wanted ignorespace %v", zy.IgnoreSpace, want.ignoreSpace)
+	}
+
+}
+
+func TestNewZyph(t *testing.T) {
+	zy := NewZypher()
+	want := wantZyph(3, 3, 3, false, false)
+	if zy.Shift != want.shift {
+		t.Errorf("got shift %q, wanted shift %q", zy.Shift, want.shift)
+	}
+
+	if zy.ShiftIterCount != want.shfIterCount {
+		t.Errorf("got shfitercount %q, wanted shfitercount %q", zy.ShiftIterCount, want.shfIterCount)
+	}
+
+	if zy.HashIterCount != want.hshIterCount {
+		t.Errorf("got hshitercount %v, wanted hshitercount %v", zy.HashIterCount, want.hshIterCount)
+	}
+
+	if zy.Alternate != want.alternate {
+		t.Errorf("got alternate %v, wanted alternate %v", zy.Alternate, want.alternate)
+	}
+
+	if zy.IgnoreSpace != want.ignoreSpace {
+		t.Errorf("got ignorespace %v, wanted ignorespace %v", zy.IgnoreSpace, want.ignoreSpace)
+	}
+}
+
+func TestCustomZyph(t *testing.T) {
+	shf := 4
+	shfiter := 4
+	dfltHshIter := 3
+	alt := true
+	ign := true
+
+	zy := NewZypher(WithShift(shf), WithShiftIterCount(shfiter), WithAlternate(alt), WithIgnoreSpace(ign))
+	want := wantZyph(shf, shfiter, dfltHshIter, alt, ign)
+
+	if zy.Shift != want.shift {
+		t.Errorf("got shift %q, wanted shift %q", zy.Shift, want.shift)
+	}
+
+	if zy.ShiftIterCount != want.shfIterCount {
+		t.Errorf("got itercount %q, wanted itercount %q", zy.ShiftIterCount, want.shfIterCount)
+	}
+
+	if zy.Alternate != want.alternate {
+		t.Errorf("got alternate %v, wanted alternate %v", zy.Alternate, want.alternate)
+	}
+
+	if zy.IgnoreSpace != want.ignoreSpace {
+		t.Errorf("got ignorespace %v, wanted ignorespace %v", zy.IgnoreSpace, want.ignoreSpace)
+	}
+}
+
+func wantZyph(shf, shfItr, hshItr int, alt, ign bool) struct {
+	shift        int
+	shfIterCount int
+	hshIterCount int
+	alternate    bool
+	ignoreSpace  bool
+} {
+	return struct {
+		shift        int
+		shfIterCount int
+		hshIterCount int
+		alternate    bool
+		ignoreSpace  bool
+	}{
+		shift:        shf,
+		shfIterCount: shfItr,
+		hshIterCount: hshItr,
+		alternate:    alt,
+		ignoreSpace:  ign,
 	}
 }
